627635af765315418f10796e74d5e229cc0be0b0
1Satan3 <mega.itachi.uchiha@mail.ru>
2021-04-10 12:59:16 +0300

    Добавлена админ панель с помощью easyAdmin

---
+++ src/Controller/Admin/CommentCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CommentCrudController extends AbstractCrudController {
+    public static function getEntityFqcn(): string {
+        return Comment::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable {
+        return [
+            TextField::new('author'),
+            TextareaField::new('text'),
+            EmailField::new('email'),
+            DateTimeField::new('createdAt'),
+            TextField::new('photoFileName'),
+            AssociationField::new('conference')->setRequired(true),
+        ];
+    }
+
+}
